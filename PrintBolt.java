4a726ae6b315800fe0f987c22341d999cedfbb51
Simon Baudart <baudart.simon@gmail.com>
2021-12-13 10:57:18 +0100

    ok

---
+++ PrintBolt.java
@@ -0,0 +1,41 @@
+public class PrintBolt implements IRichBolt {
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context,
+                        OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        String sentence = input.getString(0);
+        String[] words = sentence.split(" ");
+
+        for(String word: words) {
+            word = word.trim();
+
+            if(!word.isEmpty()) {
+                word = word.toLowerCase();
+                collector.emit(new Values(word));
+            }
+
+        }
+
+        collector.ack(input);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word"));
+    }
+
+    @Override
+    public void cleanup() {}
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+
+}
